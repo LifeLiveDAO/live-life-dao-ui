9dcff934e40de3215c30827d05679004cde6c501
Firat Tale <firat.tale@auto1.com>
2022-06-08 16:19:14 +0200

    fix router

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -19,7 +19,7 @@ import { Toaster } from "react-hot-toast";
 function App() {
 	return (
 		<>
-			<BrowserRouter basename="live-life-dao-ui">
+			<BrowserRouter basename="www.lifelivedao.com">
 				<Routes>
 					<Route path="/" element={<MainPage />} />
 					<Route path="*" element={<Page404 />} />
