f72cab0ed62de910e0b5861b3d69298c5bb20c85
Firat Tale <firat.tale@auto1.com>
2022-06-14 13:49:56 +0300

    remove unneccesary component

--- packages/react-app/src/components/InvestorNfts.js
+++ packages/react-app/src/components/InvestorNfts.js
@@ -12,9 +12,9 @@ export default function InvestorNfts() {
 				<p>
 					As a sign of gratitude towards our first supporters, you will receive a Web3 Raver NFT, designed by one of our
 					founders. The NFT also represents your ticket to receive your tokens during the token generation event. Read
-					more about the seed sale in the forthcoming litepaper soon and chat with us in our Telegram group. The seed
-					sale tokens do not have economic value; it is a token that gives you early access to our governance token used
-					to steer the LifeLive DAO. It has no claim on financial rights.
+					more about the NFT ticket sale in the forthcoming litepaper soon and chat with us in our Telegram group. The
+					NFT Ticket Sale tokens do not have economic value; it is a token that gives you early access to our governance
+					token used to steer the LifeLive DAO. It has no claim on financial rights.
 				</p>
 			</div>
 			<img className="img-mint-mobile img-mint-margin" src="nft/nft1.jpg" alt="nft2" />
--- packages/react-app/src/components/MintPage/index.js
+++ packages/react-app/src/components/MintPage/index.js
@@ -1,8 +1,7 @@
 import Golden from "./Golden";
 import GuestList from "./GuestList";
 import MintInfo from "./MintInfo";
-import MintIntro from "./MintIntro";
 import OnBoard from "./OnBoard";
 import NftImage from "./NftImage";
 
-export { Golden, GuestList, MintInfo, MintIntro, OnBoard, NftImage };
+export { Golden, GuestList, MintInfo, OnBoard, NftImage };
